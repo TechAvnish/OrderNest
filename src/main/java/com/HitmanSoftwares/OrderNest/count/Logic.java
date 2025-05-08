0ae8a59117042a2b7b2c487db15e9f6e51aa0505
AVNISH KUMAR <140041775+TechAvnish@users.noreply.github.com>
2025-05-08 14:38:57 +0530

    Add files via upload

---
+++ src/main/java/com/HitmanSoftwares/OrderNest/count/Logic.java
@@ -0,0 +1,9 @@
+package com.HitmanSoftwares.OrderNest.count;
+
+public class Logic {
+    public static double countTotal(double price,int quantity)
+    {
+        double res=price*quantity;
+        return res;
+    }
+}
