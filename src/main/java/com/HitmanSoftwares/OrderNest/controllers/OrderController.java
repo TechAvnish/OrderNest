52db60176770fb749141939604c09291dcb85f20
AVNISH KUMAR <140041775+TechAvnish@users.noreply.github.com>
2025-05-08 14:37:18 +0530

    Add files via upload

---
+++ src/main/java/com/HitmanSoftwares/OrderNest/controllers/OrderController.java
@@ -0,0 +1,17 @@
+package com.HitmanSoftwares.OrderNest.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.HitmanSoftwares.OrderNest.entities.Orders;
+import com.HitmanSoftwares.OrderNest.entities.User;
+import com.HitmanSoftwares.OrderNest.services.OrderServices;
+
+@Controller
+public class OrderController {
+}
